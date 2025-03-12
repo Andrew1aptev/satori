df0c807c7bc8bab44406ce60451bd37014cb94ec
Andrew Laptev <laptewandrew@gmail.com>
2025-03-12 21:49:08 +0300

    some fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.querySelector(".hero__decoration-text").classList.add("active");
+    }, 100);
+});
 
